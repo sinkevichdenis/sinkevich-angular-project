26bd0144adaffda65a6f742390dd28769c49df3d
SinkevichDenis <rggrodno@gmail.com>
2019-10-03 08:11:48 +0300

    feat(routing): add error page

--- src/app/modules/main/components/main/main.component.ts
+++ src/app/modules/main/components/main/main.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.sass']
 })
-export class MainComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class MainComponent {
 
 }
