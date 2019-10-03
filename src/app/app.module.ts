a192ea4c06d0a12694518d1e56ccfb8ced2bc917
SinkevichDenis <rggrodno@gmail.com>
2019-10-03 16:00:56 +0300

    feat(routing): add tabs library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// main modules
 import { AppRoutingModule } from './modules/routing/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+
+
 
 // feature modules
 import { MainModule } from './modules/main/main.module';
 import { FooterModule } from './modules/footer/footer.module';
 import { HeaderModule } from './modules/header/header.module';
 import { CpaModule } from './modules/cpa/cpa.module';
+import {MatToolbarModule} from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
