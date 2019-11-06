c6f0bef2c860c1225720585da9145ad9727eaa44
SinkevichDenis <rggrodno@gmail.com>
2019-11-06 15:31:03 +0300

    feat(shared): add accordion continue

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,20 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularDateTimePickerModule } from 'angular2-datetimepicker';
+import { MatExpansionModule } from '@angular/material/expansion'
 import { DatepickerComponent } from './components/datepicker/datepicker.component';
+import { AccordionComponent } from './components/accordion/accordion.component';
 
 @NgModule({
   declarations: [
-    DatepickerComponent
+    DatepickerComponent,
+    AccordionComponent
   ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    AngularDateTimePickerModule
+    AngularDateTimePickerModule,
+    MatExpansionModule
   ],
   exports: [
     AngularDateTimePickerModule,
-    DatepickerComponent
+    DatepickerComponent,
+    AccordionComponent
   ]
 })
 export class SharedModule { }
