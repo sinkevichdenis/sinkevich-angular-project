0ef63d4e7d6c9651a3cb4a58fb12ecf9b25c112f
SinkevichDenis <rggrodno@gmail.com>
2019-11-09 06:27:17 +0300

    feat(core): add options to date-options service

--- src/app/core/services/date-options.service.ts
+++ src/app/core/services/date-options.service.ts
@@ -3,15 +3,17 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class DateOptionsService {
 
-  static getFirstDayOfWeek(): Date {
-    const date = new Date();
+  static getFirstDayOfWeek(date: Date): Date {
     const currentWeekDay = date.getDay();
     const lessDays = currentWeekDay === 0 ? 6 : currentWeekDay - 1;
     return new Date(new Date(date).setDate(date.getDate() - lessDays));
   }
 
-  static getFirstDayOfMonth(): Date {
-    const date = new Date();
+  static getFirstDayOfMonth(date: Date): Date {
     return new Date(date.getFullYear(), date.getMonth(), 1);
+  }
+
+  static getStartOfDate(date: Date): number {
+    return  new Date(date).setHours(0, 0, 0, 0);
   }
 }
