dd6132c7a5d961b7c3cb3037e3e3e3f3ee22c531
SinkevichDenis <rggrodno@gmail.com>
2019-11-09 06:28:15 +0300

    feat(balance): add balance containers

--- src/app/cpa/components/balance-container/balance-container.component.ts
+++ src/app/cpa/components/balance-container/balance-container.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/internal/operators';
+import { DateOptionsService } from '../../../core/services/date-options.service';
+import { CpaPayment } from '../../../core/models/cpa-payment.interface';
 
 @Component({
   selector: 'app-balance-container',
   templateUrl: './balance-container.component.html',
   styleUrls: ['./balance-container.component.sass']
 })
-export class BalanceContainerComponent implements OnInit {
+export class BalanceContainerComponent {
+  @Input() payments$: Observable<CpaPayment[]>;
+  readonly currDate = new Date();
+  readonly startOfDay = DateOptionsService.getStartOfDate(this.currDate);
+  readonly startOfWeek = DateOptionsService.getStartOfDate(DateOptionsService.getFirstDayOfWeek(this.currDate));
+  readonly startOfMonth = DateOptionsService.getStartOfDate(DateOptionsService.getFirstDayOfMonth(this.currDate));
 
-  constructor() { }
+  constructor(
+    private dateOptions: DateOptionsService
+  ) {}
 
-  ngOnInit() {
+  filterPayments(firstDate: number): Observable<CpaPayment[]> {
+    return this.payments$.pipe(
+      map( items => items.filter(item => item.date >= firstDate && item.date <= Date.now()))
+    );
   }
 
+  reducePayments(firstDate: number): Observable<number> {
+    return this.filterPayments(firstDate).pipe(
+      map( items => items.reduce( (acc, curr) => acc + curr.value, 0))
+    );
+  }
 }
